0b845201e0329a78b85896c677f63a923eca2dc1
Alejo1120 <cedenoalejandro117@gmail.com>
2021-07-25 13:58:10 -0500

    conectamos base de datos de mongo

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
   const mongoose = require ('mongoose'); //requerimos mongoose para la base de datos
 
 
+  mongoose.connect('mongodb+srv://Alejandro:1007703016@cluster0.fok3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+
+    useNewUrlParser : true , useUnifiedTopology: true,
+  }). then(db => console.log('base de datos formulario conectada'))
+  .catch(err=>console.log (err))
 
 //que nuestro servidor escoja el puerto en el que corra en caso de que no, se ejecuta en el puerton 3000
 app.set ('port', process.env.PORT || 8080); 
